43f77818c25590ef430ec4e645029b01cd20305a
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2023-05-10 15:43:55 -0400

    Templatize plugin arg string, add some vault checking methods

--- pfbridge/config/settings.py
+++ pfbridge/config/settings.py
@@ -1,6 +1,8 @@
 import  os
-from    pydantic    import BaseSettings
+from typing import Any
+from    pydantic    import AnyHttpUrl, BaseSettings, AnyUrl
 from    models      import relayModel
+from    pftag       import pftag
 
 class Pflink(BaseSettings):
     prodURL:str             = 'http://localhost:8050/api/v1/workflow'
@@ -10,9 +12,13 @@ class DylldAnalysis(Pflink):
     pipelineName:str        = ''
     pluginName:str          = 'pl-dylld'
     pluginVersion:str       = '4.4.28'
-    pluginArgs:str          = '--pattern **/*dcm --CUBEurl http://cube-next.tch.harvard.edu/api/v1/ --CUBEuser %usernameCUBE --CUBEpassword %passwordCUBE --orthancURL http://10.72.8.224:8042 --orthancuser %usernameOrthanc --orthancpassword %passwordOrthanc'
+    pluginArgs:str          = '--pattern **/*dcm --CUBEurl %urlCUBE --CUBEuser %usernameCUBE --CUBEpassword %passwordCUBE --orthancURL %urlOrthanc --orthancuser %usernameOrthanc --orthancpassword %passwordOrthanc'
     clinicalUser:str        = '%usernameCUBE'
     feedName:str            = 'dylld-%SeriesInstanceUID'
+
+class ServiceURLs(BaseSettings):
+    urlCUBE:str             = "http://localhost:8000/api/v1/"
+    urlOrthanc:str          = "http://localhost:8888"
 
 class Vault(BaseSettings):
     locked:bool             = False
@@ -26,8 +32,40 @@ class CredentialsOrthanc(BaseSettings):
     usernameOrthanc:str     = ''
     passwordOrthanc:str     = ''
 
+def vaultCheckLock(vault:Vault) -> None:
+    if vault.vaultKey and not vault.locked:
+        vault.locked        = True
+        print("Vault check: key has already been set. Vault is now LOCKED.")
+
+def analysis_decode() -> None:
+    decode:pftag.Pftag  = pftag.Pftag({})
+    addDict:bool = decode.lookupDict_add(
+        [
+            {
+                'CUBE': {
+                    'usernameCUBE': credentialsCUBE.usernameCUBE,
+                    'passwordCUBE': credentialsCUBE.passwordCUBE,
+                    'urlCUBE':      serviceURLs.urlCUBE
+                }
+            },
+            {
+                'orthanc': {
+                    'usernameOrthanc':  credentialsOrthanc.usernameOrthanc,
+                    'passwordOrthanc':  credentialsOrthanc.passwordOrthanc,
+                    'urlOrthanc':       serviceURLs.urlOrthanc
+                }
+            }
+        ]
+    )
+    for field in ['pluginArgs', 'clinicalUser']:
+        d_decode:dict = decode(analysis.__getattribute__(field))
+        if d_decode['status']:
+            analysisDecoded.__setattr__(field, d_decode['result'])
+
 pflink              = Pflink()
 analysis            = DylldAnalysis()
+analysisDecoded     = DylldAnalysis()
 vault               = Vault()
 credentialsCUBE     = CredentialsCUBE()
 credentialsOrthanc  = CredentialsOrthanc()
+serviceURLs         = ServiceURLs()
